2211d592e97d87a9ec8b91fbd6a7ed52dbf7c5c0
mara <mara@local>
2024-11-29 00:19:34 +0100

    FIXME: Probably an odd address in characters masks initialization.

--- sprite.h
+++ sprite.h
@@ -1,6 +1,8 @@
 #ifndef __SV2024SPRITE_H
 #define __SV2024SPRITE_H 1
 
+void init_font_mask();
+
 void update_sprite(unsigned short* video_ptr,
                    unsigned short* background_ptr,
                    unsigned short clk);
