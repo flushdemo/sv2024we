f43dedcf4f9d1af3f57f89badf16133e76834c48
mara <mara@local>
2024-11-30 20:57:33 +0100

    only 1 plane mask.

--- sprite.s
+++ sprite.s
@@ -3,74 +3,38 @@
 	public	_update_sprite
 
 _init_sprite:
-    movem.l     d0-d7/a0-a6,-(a7)
+     ;movem.l     d0-d7/a0-a6,-(a7)
     ; collect the palette data
-    moveq.l     #0,d0
-    moveq.l     #0,d1
-    moveq.l     #0,d2
-    moveq.l     #0,d3
-    moveq.l     #0,d4
-    moveq.l     #0,d5
-    moveq.l     #0,d6
-    moveq.l     #0,d7
-    movea.l     #0,a0
-    movea.l     #0,a1
-    movea.l     #0,a2
-    movea.l     #0,a3
-    movea.l     #0,a4
-    movea.l     #0,a5
-    movea.l     #0,a6
 
     lea         garray,a6
     lea         gmskarray,a5
     moveq.l     #8-1,d7
+
 loop_index_spr:
     move.l      (a6)+,a4     ; a4 pointer to sprite
     move.l      (a5)+,a3     ; a3 pointer to computed mask
-    move.l      #175-1,d6
+    move.l      #9800/8-1,d6
+
 loop_msk_compute:
-    moveq.l     #0,d0
+    moveq.l     #0,d1
     moveq.l     #0,d2
     moveq.l     #0,d3
     moveq.l     #0,d4
 
 
 ; 1 block of 16 pixels
-	move.w      0(a4),d0
+	move.w      0(a4),d1
 	move.w      2(a4),d2
     move.w      4(a4),d3
     move.w      6(a4),d4
 
-    ; plane 1
-	or.w        d0,0(a3)
-	or.w        d2,0(a3)
-	or.w        d3,0(a3)
-	or.w        d4,0(a3)
-	not.w       0(a3)
-
-    ; plane 2
-	or.w        d0,2(a3)
-	or.w        d2,2(a3)
-	or.w        d3,2(a3)
-	or.w        d4,2(a3)
-	not.w       2(a3)
-
-	; plane 3
-	or.w        d0,4(a3)
-	or.w        d2,4(a3)
-	or.w        d3,4(a3)
-	or.w        d4,4(a3)
-	not.w       4(a3)
-
-	; plane 4
-	or.w        d0,6(a3)
-	or.w        d2,6(a3)
-	or.w        d3,6(a3)
-	or.w        d4,6(a3)
-	not.w       6(a3)
+	or.w        d1,d2
+	or.w        d2,d3
+	or.w        d3,d4
+	not.w       d4
+	move.w      d4,(a3)+
 
     lea         8(a4),a4
-    lea         8(a3),a3
 
     dbf         d6,loop_msk_compute
     dbf         d7,loop_index_spr
@@ -78,6 +42,7 @@ loop_msk_compute:
 
     movem.l     (a7)+,d0-d7/a0-a6
     rts
+
 
 
 _update_sprite:
@@ -98,7 +63,7 @@ gnome_show:
 loop_characters:
 	; show gnome frame1 mask
 
-	movea.l	a6,a1		; a1 video ptr
+	movea.l	a5,a1		; a1 video ptr
 	lea	16*160(a1),a1
 	move.l #175-1,d2
 	; d1 oqp
@@ -116,14 +81,21 @@ loop_characters:
 	; ; movem.l	(a0)+,d2-d5
 	; ; movem.l	d2-d5,(a1)
 	; ; lea  16(a1),a1
-	REPT 2*7
-	move.l	(a4)+,d7		; copie du masque
-	and.l	(a1),d7
-	move.l	d7,(a1)
-	move.l	(a0)+,d6		; copie du sprite
-	or.l	(a1),d6
-	move.l	d6,(a1)+
+	REPT 7
+    move.w      (a4),d1
+    move.l      (a0),d4
+    move.l      4(a0),d3
+    and.w       d1,0(a1)
+    and.w       d1,2(a1)
+    and.w       d1,4(a1)
+    and.w       d1,6(a1)
+    or.l        d4,0(a1)
+    or.l        d3,4(a1)
+    lea         8(a0),a0
+    lea         2(a4),a4
+    lea         8(a1),a1
 	ENDR
+	
 	add.l	#160-(7*8),a1        ; 7 blocks of 16pixels
 	dbf d2,.shw_gnome_msk_frame_0
 
@@ -151,11 +123,11 @@ g7f			incbin '.\g7spr.bin' ; the data of gnome and background for frame 1
 
         section bss
 
-g0msk		dcb.b 9800,$0
-g1msk		dcb.b 9800,$0
-g2msk		dcb.b 9800,$0
-g3msk		dcb.b 9800,$0
-g4msk		dcb.b 9800,$0
-g5msk		dcb.b 9800,$0
-g6msk		dcb.b 9800,$0
-g7msk		dcb.b 9800,$0
+g0msk		dcb.b 2450
+g1msk		dcb.b 2450
+g2msk		dcb.b 2450
+g3msk		dcb.b 2450
+g4msk		dcb.b 2450
+g5msk		dcb.b 2450
+g6msk		dcb.b 2450
+g7msk		dcb.b 2450
